6a22411eb9776690036f11620bba82e46bec839b
Moses Marsh <mosesmarsh@gmail.com>
2017-03-03 16:51:28 -0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -44,6 +44,7 @@ echo 'export PATH="~/Applications/Postgres.app/Contents/Versions/latest/bin/:$PA
 # installing anaconda
 curl -L https://repo.continuum.io/archive/Anaconda2-4.3.0-MacOSX-x86_64.sh > anaconda_script.sh
 bash anaconda_script.sh -b
+echo "export PATH="/Users/datascientist/anaconda2/bin:$PATH"" >> ~/.bash_profile
 
 rm ~/Desktop/Launcher.app
 
