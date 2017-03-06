301e02076f8170ca980b3673639ad3c5cb6dfbb1
Moses Marsh <mosesmarsh@gmail.com>
2017-03-06 10:53:38 -0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -11,7 +11,8 @@ sudo rm -rf /data/db
 echo "Clean up home directory"
 shopt -s extglob
 cd ~
-mv .bash_profile .backup_bash_profile
+mv .bash_profile backup_bash_profile
+rm -rf ~/.*
 touch .bash_profile
 sudo rm -rdf !(Applications|Library|Desktop|Documents|Downloads|Pictures|Public|mac_cleanup)
 cd -
