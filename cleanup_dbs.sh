22a02c2a5a219ba87963644488486c394547bef1
Moses Marsh <mosesmarsh@gmail.com>
2017-03-08 15:56:56 -0800

    Update cleanup_dbs.sh

--- cleanup_dbs.sh
+++ cleanup_dbs.sh
@@ -1,7 +1,7 @@
 DBS=`psql -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;"`
 for arg in $DBS
 do
-  if [ "$arg" != "postgres" ] && [ "$arg" != "gSchool" ] && [ "$arg" != "Gschoolstudent" ]
+  if [ "$arg" != "postgres" ] && [ "$arg" != "gSchool" ] && [ "$arg" != "Gschoolstudent" ] && [ "$arg" != "gschoolstudent" ]
   then
     `dropdb $arg`
   else
